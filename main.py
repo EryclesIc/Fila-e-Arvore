7cfc7f423f88dd07244c9911dc6d62f87655fa90
Flavia Soares <flaviasoares@ic.ufal.br>
2020-11-19 03:37:12 -0300

    finalizando

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from estruturaFila import Queue
-from estruturaArvore import BSTNode
+from estruturaArvore import BinaryTree
 import pandas as pd
 import random
 
-# arvore = BinaryTree()
-ROOT = "root"
-# node = Node()
 fila = Queue()
 
 # efetua a leitura do csv com a biblioteca pandas
@@ -17,19 +14,24 @@ total_cols = len(df.columns)
 print("o csv tem {} linhas e {} colunas".format(total_rows, total_cols))
 
 # utiliza a biblioteca random para sortear um número aleatório de linhas e colunas
-tamanhoArvore = 0 # inicializando o contador de nós dentro da árvore
 coluna_sorteada = random.randint(0, total_cols)-1
 for i in range(3):
     linha_sorteada = random.randint(0, total_rows)-1
     
     # salva o dado da linha e coluna sorteada em uma variável
     elemento = df.iloc[linha_sorteada][coluna_sorteada]
-    # print(elemento)
-
-    if i == 0: arvore = BSTNode(linha_sorteada, elemento)
+    fila.push(elemento)
+    
+    if i == 0: arvore = BinaryTree(linha_sorteada, elemento)
     else:
-        node = BSTNode(linha_sorteada, elemento)
+        node = BinaryTree(linha_sorteada, elemento)
         arvore.add(node)
+
+fila.pop()
+item = fila.peek()
+print(item)
+
+
 arvore.traverse()
 print("-------------------------------------------")
 chave = int(input())
